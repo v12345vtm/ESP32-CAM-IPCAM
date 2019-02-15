f676b581159030ba3dae802c9263a1999b0495c9
v12345vtm <vithprodlet@gmail.com>
2019-02-15 17:59:45 +0100

    Create getIPCAMimage.php

---
+++ php/getIPCAMimage.php
@@ -0,0 +1,26 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+
+</head>
+<body>
+
+<?php include 'copyfoto.php';?>
+
+
+<h1>My First fotocopy</h1>
+ <img src="http://192.168.1.20/capture" alt="error"   width="200" height="200"> 
+<p>My first paragraph.</p>
+
+
+ 
+<?php include 'footer.php';?>
+</body>
+</html> 
+
+
+
+
+
+
